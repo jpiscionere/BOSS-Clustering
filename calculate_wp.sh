ee2e819e0be07ccbc07cbd46299f4f4248a0b09a
Jennifer Piscionere <jpiscionere@gmail.com>
2013-07-30 12:47:08 -0500

    test

--- calculate_wp.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-./calculate_wp bin1_DsDi_with_norm.new.out bin1_DsRi_with_norm.new.out bin1_RsDi_with_norm.new.out bin1_RsRi_with_norm.new.out 20 100 0.0002114131 1  bin1_covar_inv_with_norm.new.out >bin1_wp_with_norm.new.out
-./calculate_wp bin2_DsDi_with_norm.new.out bin2_DsRi_with_norm.new.out bin2_RsDi_with_norm.new.out bin2_RsRi_with_norm.new.out 20 100 0.0002114131 1  bin2_covar_inv_with_norm.new.out >bin2_wp_with_norm.new.out
-./calculate_wp bin3_DsDi_with_norm.new.out bin3_DsRi_with_norm.new.out bin3_RsDi_with_norm.new.out bin3_RsRi_with_norm.new.out 20 100 0.0002114131 1  bin3_covar_inv_with_norm.new.out >bin3_wp_with_norm.new.out
-./calculate_wp bin4_DsDi_with_norm.new.out bin4_DsRi_with_norm.new.out bin4_RsDi_with_norm.new.out bin4_RsRi_with_norm.new.out 20 100 0.0002114131 1  bin4_covar_inv_with_norm.new.out >bin4_wp_with_norm.new.out
