6e5588afdf546ec5bb637e1feee015e96993cd0b
Jennifer Piscionere <jpiscionere@gmail.com>
2015-03-23 13:22:19 -0500

    jackknife dr3 test

--- Scripts/run_eyal_jack.sh
+++ Scripts/run_eyal_jack.sh
@@ -4,12 +4,17 @@ Di=/hd0/Research/Clustering/Boss/DR3/dr3_Di_mag_extinction
 Ri=/hd0/Research/Clustering/Boss/DR3/dr3_Ri_short
 mask=/hd0/Research/Clustering/Boss/DR3/window.sample14.trimmed.ply
 
-nbins=nbins
+Ds=dr3_Ds_zrange_trim
+Di=dr3_Di_mag_extinction
+mask=window.sample14.trimmed.ply 
+
+nbins=17
 area_tot=0.9076429
 r_min=0.005
 n_jack=150
 
-~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Ds $Di $mask outfile_DsDi r_min 10 nbins $n_jack 1 >dr3_DsDi_jackknife
-~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Ds $Ri $mask outfile_DsDi r_min 10 nbins $n_jack 2 >dr3_DsRi_jackknife
-~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Rs $Di $mask outfile_DsDi r_min 10 nbins $n_jack 1 >dr3_RsDi_jackknife
-~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Rs $Ri $mask outfile_DsDi r_min 10 nbins $n_jack 2 >dr3_RsRi_jackknife
+
+#~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Ds $Di $mask outfile_DsDi $r_min 10 $nbins $n_jack 1 >dr3_DsDi_jackknife
+~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Ds $Ri $mask outfile_DsDi $r_min 10 $nbins $n_jack 2 >dr3_DsRi_jackknife
+#~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Rs $Di $mask outfile_DsDi $r_min 10 $nbins $n_jack 1 >dr3_RsDi_jackknife
+#~/Clustering/Boss/Source/Vpac_Codes/measure_Boss_wp_openmp_mag_cut $Rs $Ri $mask outfile_DsDi $r_min 10 $nbins $n_jack 2 >dr3_RsRi_jackknife
