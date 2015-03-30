2ae3008136b629aef024c54dfffd56b41c233620
Jennifer Piscionere <jpiscionere@gmail.com>
2015-03-30 14:34:16 -0500

    calculate_wp_nojack.sh

--- Scripts/calculate_wp_nojack.sh
+++ Scripts/calculate_wp_nojack.sh
@@ -1,15 +1,35 @@
 #! /bin/bash
 
-nden=0.0001260
-echo $nden
-
-nden==0.0000892803189156
 
 
-paste dr3_DsDi_mag_cut dr3_DsRi_mag_cut dr3_RsDi_mag_cut dr3_RsRi_mag_cut >tmp 
-awk '{wp=$2/$5-$8/$11; {print $1,wp,wp/'$nden'}}'<tmp >wp_dr3_mag_cut
+#nden[1]=0.0002569
+#nden[2]=0.0003243
+#nden[3]=0.0002327
+#nden[4]=0.0000942
+#nden[5]=0.0001848
 
 
+
+nden[1]=0.0002494737166611
+nden[2]=0.0003150319712696
+nden[3]=0.0002238406773599
+nden[4]=0.0000912964784524
+nden[5]=0.0001847687542961
+
+i=1
+
+for bin in bin1 bin2 bin3 bin4 bin_all 
+do
+
+nden1=${nden[$i]}
+echo $nden1
+
+paste ${bin}_DsDi_nojack.out ${bin}_DsRi_nojack.out ${bin}_RsDi_nojack.out ${bin}_RsRi_nojack.out >tmp 
+awk '{wp=$2/$5-$8/$11; {print $1,wp,wp/'$nden1'}}'<tmp >wp_${bin}_nojack.out
+
+i=`expr $i + 1`
+
+done
 
 
 
