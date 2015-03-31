3a618adcf1d2320f102c62284a24c7ae7d85ef22
Jennifer Piscionere <jpiscionere@gmail.com>
2015-03-31 15:21:56 -0500

    plot_color_mag_smooth.r

--- Plotting/plot_color_mag_smooth.r
+++ Plotting/plot_color_mag_smooth.r
@@ -27,8 +27,12 @@ i_dperp<- i_dperp + geom_abline(intercept=-11.575,slope=0.625)
 i_dperp<- i_dperp + stat_smooth(aes(fill=bin,col=bin)) +scale_colour_hue(guide="none")  + scale_fill_discrete(name="Redshift Sample\n",breaks=c("Bin1","Bin2","Bin3","Bin4"),labels=c("0.5 > z > 0.43","0.55 > z > 0.5","0.6 > z > 0.55","0.7 > z > 0.6" ))
 
 
+
 gr_ri<-ggplot(subset(frame,!is.na(bin)),aes(x=r_i,y=g_r)) + ylab(expression(r[mod] - i[mod])) + xlab(expression(g[mod] - r[mod])) + geom_point(color="grey",alpha=0.1,shape=".") + theme_bw() + theme(axis.title.y=element_text(size=20,angle=90,face="italic"),axis.title.x=element_text(size=20,vjust=-0.2),axis.text=element_text(size=20),panel.border=element_rect(size=1.1),legend.position="none") + ylim(0.2,1.5)
 
 gr_ri<- gr_ri + geom_abline(intercept=0.6,slope=1/8)
 
 gr_ri<- gr_ri + stat_smooth(aes(fill=bin,col=bin),show_guide=FALSE)
+
+grid.arrange(gr_ri + theme(plot.margin=unit(c(0,1,0.5,2),"cm")),i_dperp + theme(plot.margin=unit(c(0,-1,0.5,0.5),"cm")),nrow=2,ncol=2)
+
