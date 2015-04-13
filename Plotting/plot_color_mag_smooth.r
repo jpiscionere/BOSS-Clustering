cc2819653b7f601778909becb6d1bd0da3e832f0
Jennifer Piscionere <jpiscionere@gmail.com>
2015-04-13 17:26:45 -0500

    plot_color_mag_smooth.r

--- Plotting/plot_color_mag_smooth.r
+++ Plotting/plot_color_mag_smooth.r
@@ -25,14 +25,29 @@ i_dperp<-ggplot(subset(frame,!is.na(bin)),aes(x=i_cmod,y=dperp,group=bin))  + yl
 i_dperp<- i_dperp + geom_hline(aes(yintercept=0.55))
 i_dperp<- i_dperp + geom_abline(intercept=-11.575,slope=0.625)
 i_dperp<- i_dperp + stat_smooth(aes(fill=bin,col=bin)) +scale_colour_hue(guide="none")  + scale_fill_discrete(name="Redshift Sample\n",breaks=c("Bin1","Bin2","Bin3","Bin4"),labels=c("0.5 > z > 0.43","0.55 > z > 0.5","0.6 > z > 0.55","0.7 > z > 0.6" ))
-
+i_dperp<- i_dperp + theme(legend.position=c(.25, .85),legend.text=element_text(size=15),legend.title=element_text(size=15))
 
 
 gr_ri<-ggplot(subset(frame,!is.na(bin)),aes(x=r_i,y=g_r)) + ylab(expression(r[mod] - i[mod])) + xlab(expression(g[mod] - r[mod])) + geom_point(color="grey",alpha=0.1,shape=".") + theme_bw() + theme(axis.title.y=element_text(size=20,angle=90,face="italic"),axis.title.x=element_text(size=20,vjust=-0.2),axis.text=element_text(size=20),panel.border=element_rect(size=1.1),legend.position="none") + ylim(0.2,1.5)
 
 gr_ri<- gr_ri + geom_abline(intercept=0.6,slope=1/8)
 
-gr_ri<- gr_ri + stat_smooth(aes(fill=bin,col=bin),show_guide=FALSE)
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
+cols=gg_color_hue(4)
+
+
+
+
+gr_ri_bin1<- gr_ri + stat_smooth(data=subset(frame,bin=="Bin1"),aes(fill=bin,col=cols[1]),show_guide=FALSE)
+gr_ri_bin2<- gr_ri + stat_smooth(data=subset(frame,bin=="Bin2"),aes(fill=bin,col=cols[2]),show_guide=FALSE)
+gr_ri_bin3<- gr_ri + stat_smooth(data=subset(frame,bin=="Bin3"),aes(fill=bin,col=cols[3]),show_guide=FALSE)
+gr_ri_bin4<- gr_ri + stat_smooth(data=subset(frame,bin=="Bin4"),aes(fill=bin,col=cols[4]),show_guide=FALSE)
+ 
+
+grid.arrange(gr_ri_bin1 + theme(plot.margin=unit(c(0,1,0.5,2),"cm")),gr_ri_bin2,gr_ri_bin3,gr_ri_bin4)
 
 grid.arrange(gr_ri + theme(plot.margin=unit(c(0,1,0.5,2),"cm")),i_dperp + theme(plot.margin=unit(c(0,-1,0.5,0.5),"cm")),nrow=2,ncol=2)
 
