a467d654155e7be43c0f5c49c3c9369cf0d1fd54
Jennifer Piscionere <jpiscionere@gmail.com>
2015-04-15 15:58:02 -0500

    plot_all_one_plot.r

--- Plotting/plot_all_one_plot.r
+++ Plotting/plot_all_one_plot.r
@@ -16,23 +16,35 @@ plot.window(xlab=NA,ylab=NA,ylim=ylims, cex.lab=1, cex.axis=3, cex.main=1.5, cex
 
 
 
-data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin1_nojack.out")
+data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin1_nojack_selection.out")
 wp=data.frame(data)
 #bins2=(data$V1[c(1:17)]+data$V1[1+c(1:17)])/2
 
 plot(data$V1,data$V3,log="xy",axes=FALSE,type="l",col="deeppink",xlab=NA,ylab=NA,ylim=ylims, cex.lab=1,lwd=2, cex.axis=3, cex.main=1.5, cex.sub=1.5,main="BOSS CMASS")
 #errbar(data$V1,wp$V3,wp$V3+wp$V4,wp$V3-wp$V4,log="xy",add=TRUE,col="dodgerblue")
 
+#data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin1_nojack.out")
+#lines(data$V1,data$V3*data$V1,col="deeppink",lwd=2,lty=2)
 
-data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin2_nojack.out")
+data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin2_nojack_selection.out")
 lines(data$V1,data$V3,col="dodgerblue",lwd=2)
 
+#data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin2_nojack.out")
+#lines(data$V1,data$V3*data$V1,col="dodgerblue",lwd=2,lty=2)
 
-data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin3_nojack.out")
+data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin3_nojack_selection.out")
 lines(data$V1,data$V3,col="purple",lwd=2)
 
-data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin4_nojack.out")
+#data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin3_nojack.out")
+#lines(data$V1,data$V3*data$V1,col="purple",lwd=2,lty=2)
+
+
+data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin4_nojack_selection.out")
 lines(data$V1,data$V3,col="seagreen2",lwd=2)
+
+#data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin4_nojack.out")
+#lines(data$V1,data$V3*data$V1,col="seagreen2",lwd=2,lty=2)
+
 
 data=read.table("/hd0/Research/Clustering/Boss/dr11/dr11v2/wp_bin_all_nojack.out")
 lines(data$V1,data$V3,lwd=2)
