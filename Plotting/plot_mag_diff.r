bd76ab9a59587f2f9d4f5e51b9bf57062595baa8
Jennifer Piscionere <jpiscionere@gmail.com>
2015-04-30 15:00:55 -0500

    plot_mag_diff.r

---
+++ Plotting/plot_mag_diff.r
@@ -0,0 +1,4 @@
+data=read.table("cross_match.out",head=T)
+frame=data.frame(petro=data$petroMag_G,diff=(data$petroMag_G-data$Mag_G)/data$Mag_G)
+c<-ggplot(frame,aes(petro,diff))
+c + stat_smooth() + geom_point(alpha=0.2,aes(colour=diff)) + ylim(-0.05,0.025) + xlim(-23.2,-20.8)+ scale_colour_gradient(low = "blue",high="red") + xlab("Calculated Mag_G") + ylab("% diff") + labs(title="Comparing Given Mag G to Calculated M_G DR7") + theme(legend.position=c(0,-21)) + geom_vline(xintercept=-21.2)
